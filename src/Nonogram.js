a91da7b585387f667ca7670e319c411e7551c9ef
maria-roman-garcia <mromangarcia04@gmail.com>
2020-03-11 21:34:45 +0100

    calculamos las pistas verticles de forma aleatoria

--- src/Nonogram.js
+++ src/Nonogram.js
@@ -1,11 +1,45 @@
-import React, {Component} from 'react';
-import './Nonogram.scss'
+import React, { Component } from 'react';
+import './Nonogram.scss';
 
-class Nonogram extends Component{
-    render(){
-        return(
-            <div>
-                <p>holisss</p>
+class Nonogram extends Component {
+
+    state = {
+        // We create a new grid here of 5x5
+        grid: [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]
+        ]
+    }
+
+    createNewGame = () => {
+        // first we calculate how many numbers are we going to have per row
+        let numbersToPlay = this.state.grid[0].length;
+
+        let verticalClues = [];
+        let horizontalClues = [];
+
+        // To calculate the vertical clues, we use a random number
+        for (let i = 0; i < numbersToPlay; i++) {
+            verticalClues.push(Math.floor(Math.random() * numbersToPlay));
+        }
+        console.log(verticalClues);
+       
+        // Esto lo he visto en internet Javi, puede estar muy chulo para iterar en cada row y dentro de cada row, en cada item...
+        //     let newPuzzle = () => {
+        //     puzzleGrid.map(row =>
+        //         row.map(item => (Math.floor(Math.random() * 3) === 0 ? 0 : 1))
+        //     )
+        // };
+    }
+
+    render() {
+        return (
+            <div className="game container-fluid" >
+                <p>{this.numbersToPlay}</p>
+                {this.createNewGame()}
             </div>
         )
     }
