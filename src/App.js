34bbd77a35d0cf3ec0d21f5c8ff0c6083e517690
maria-roman-garcia <mromangarcia04@gmail.com>
2020-03-11 19:58:24 +0100

    instalamos scss y comprobamos que funciona. Tambien probamos a importar el componente nonogram a app y vemos que todo esta conectado bien

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { Button } from 'reactstrap';
+import Nonogram from './Nonogram.js'
 
 function App() {
   return (
     <div className="App">
+      <Nonogram/>
       <Button color="danger">Danger!</Button>
     </div>
   );
---
+++ src/Nonogram.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Nonogram.scss'
+
+class Nonogram extends Component{
+    render(){
+        return(
+            <div>
+                <p>holisss</p>
+            </div>
+        )
+    }
+}
+
+export default Nonogram;
