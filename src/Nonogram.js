3152f37961cffe6887d291f6e79fa68c59a6d70e
MariaRomanDelonia <maria.roman@delonia.es>
2020-03-18 09:38:55 +0100

    cambiamos el value del cell a 0/1/2 en vez de true false. Y cambiamos el estilo en funcion de que este seleccionado

--- src/Nonogram.js
+++ src/Nonogram.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Nonogram.scss';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 
 function Nonogram() {
 
@@ -37,21 +37,44 @@ function Nonogram() {
     })
     // ---END OF STATES---
 
+    // Change the value of the cell 1/2/3 when click on it and change the state
     let changeCellValue = (x,y) => {
         let objeto = {...solutionUser};
-        objeto.grid[x][y] = !objeto.grid[x][y];
+        objeto.grid[x][y] = objeto.grid[x][y]===2 ?
+            0 : objeto.grid[x][y]+1;
+        console.log(objeto)
         changeSolutionUser(objeto);
     }
 
-    // We change the value of SolutionGame, making random values 1 or
-    useEffect(() => { changeSolutionGame(solutionGame.grid.map(row => row.map(item => item = (!!Math.round(Math.random()))))) }, [])
-    console.log({ solutionGame })
+    // We change the value of SolutionGame, making random values true/false we convert !!1 = true
+    useEffect(() => { changeSolutionGame({grid: solutionGame.grid.map(row => row.map(item => item = (Math.round(Math.random()))))}) }, [])
+    
+    // ESTO ESTA FALLANDO...Seguir mirando
+    // let sumaDePistas=()=>{
+    //     // SUMA PISTAS VERTICALES
+    //     console.log(solutionGame)
+    //     let prueba
+    //     let pistaVertical = solutionGame.grid.map(row => row.map(item =>
+    //        item ? [prueba+1] : console.log('pruebafalse')
+    //     ));
+    //     console.log(pistaVertical)
+    //     console.log(prueba)
+    //     // SUMA PISTAS HORIZONTALES. Sumamos [i] de cada array
+    //     var r = [];
+    //     for (let i = 0; i < solutionGame.grid[0]; i++) {
+    //         // solutionGame.grid.map(row => row[i])
+    //         r[i] = solutionGame.grid[i] + solutionGame.grid[i];
+    //     }
+    //     console.log(r);
+    // }
+    // setTimeout(function () { sumaDePistas() }, 3000);
+    //FIN -  ESTO ESTA FALLANDO
 
     return (
         <table className="center">
             <tbody>
                 <tr>
-                    {/* El primero va vacio */}
+                    {/*The first one goes empty*/}
                     <td></td>
                     <td>pista</td>
                     <td>pista</td>
@@ -63,7 +86,7 @@ function Nonogram() {
                     solutionUser.grid.map((row, rowIndex) => (
                         <tr key={rowIndex}>
                             <td>pista</td>
-                            {row.map((cell, cellIndex) => <td className="cell" key={cellIndex} onClick={() => changeCellValue(rowIndex, cellIndex)} >{cell? 'X' : 'O'}</td>)}
+                            {row.map((cell, cellIndex) => <td className={`cell_${cell}`} key={cellIndex} onClick={() => changeCellValue(rowIndex, cellIndex)} >{cell}</td>)}
                         </tr>
                     ))
                 }
